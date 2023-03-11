cd603f1da711f4625b66ad05260dd181fd944133
Rares Bites <R.Bites@student.tudelft.nl>
2023-03-11 19:26:29 +0100

    - Improved style

--- client/src/main/java/client/scenes/BoardCtrl.java
+++ client/src/main/java/client/scenes/BoardCtrl.java
@@ -26,7 +26,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
