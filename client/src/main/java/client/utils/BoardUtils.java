4e837c3b6c4d817d750424363d0578a0101ca586
ttanasescu <t.i.tanasescu-1@student.tudelft.nl>
2023-03-13 20:01:32 +0100

    Added BoardUtils, BoardController classes and BoardRepository

---
+++ client/src/main/java/client/utils/BoardUtils.java
@@ -0,0 +1,41 @@
+package client.utils;
+
+import commons.Board;
+import commons.Route;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.GenericType;
+import org.glassfish.jersey.client.ClientConfig;
+
+import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+
+public class BoardUtils {
+    /**
+     * Sends a post request to the server to add a board to the database
+     * @param board The board to add
+     * @return The added board
+     */
+    public Board addBoard(Board board)
+    {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(ServerUtils.SERVER).path(Route.BOARD)
+                .request(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .post(Entity.entity(board, APPLICATION_JSON), Board.class);
+    }
+
+    /**
+     * Sends a request to the server to get all boards from the database
+     * @return List of all boards in the database
+     */
+    public List<Board> getBoards()
+    {
+        return ClientBuilder.newClient(new ClientConfig()) //
+                .target(ServerUtils.SERVER).path(Route.BOARD) //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON) //
+                .get(new GenericType<List<Board>>() {});
+    }
+}
