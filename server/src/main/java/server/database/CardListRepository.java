6762fbaa0ad83e9d19178759f137c5fa282ade28
Filip-Andrei Cirtog <f.a.cirtog@student.tudelft.nl>
2023-03-21 00:00:58 +0100

    Created the CardList Controller and Repository in order to finish the database connection.

---
+++ server/src/main/java/server/database/CardListRepository.java
@@ -0,0 +1,9 @@
+package server.database;
+
+import commons.CardList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardListRepository extends JpaRepository<CardList, Long> {
+}
