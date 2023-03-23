c8cd88184dc626b022a4a359bccb14e82b5e24bd
Ruben Schnell <r.h.schnell@student.tudelft.nl>
2023-03-23 19:13:46 +0100

    Update ListModules.java

--- client/src/main/java/client/ListModules.java
+++ client/src/main/java/client/ListModules.java
@@ -17,7 +17,7 @@ package client;
 
 import client.scenes.CardWindows.AddCardCtrl;
 import client.scenes.CardWindows.ViewCardCtrl;
-import client.scenes.ListManagement.CustomListCellCtrl;
+import client.scenes.ListManagement.ListCellCtrl;
 import client.scenes.ListManagement.ListCtrl;
 import client.scenes.TagManagement.CustomTagCellCtrl;
 import client.scenes.TagManagement.TagListCtrl;
@@ -32,7 +32,7 @@ public class ListModules implements Module {
         binder.bind(ListCtrl.class).in(Scopes.SINGLETON);
         binder.bind(AddCardCtrl.class).in(Scopes.SINGLETON);
         binder.bind(ViewCardCtrl.class).in(Scopes.SINGLETON);
-        binder.bind(CustomListCellCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(ListCellCtrl.class).in(Scopes.SINGLETON);
         binder.bind(TagListCtrl.class).in(Scopes.SINGLETON);
         binder.bind(CustomTagCellCtrl.class).in(Scopes.SINGLETON);
     }
