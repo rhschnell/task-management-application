bd54ffda2d6cb1af2d9f181d0c9379c88025c0b3
Ruben Schnell <r.h.schnell@student.tudelft.nl>
2023-03-28 14:49:15 +0200

    Added null in BoardTest.java and BoardControllerTest.java as a 4th parameter because a board now takes a list of tags as the 4th parameter.

--- commons/src/test/java/commons/BoardTest.java
+++ commons/src/test/java/commons/BoardTest.java
@@ -18,7 +18,7 @@ class BoardTest {
 
     @BeforeEach
     void setUp() {
-        board = new Board("000000", "My Board", null);
+        board = new Board("000000", "My Board", null, null);
         cardList = new CardList();
         Card card = new Card(
                 "My Card",
@@ -28,7 +28,7 @@ class BoardTest {
                 null
         );
         cardList.addCard(card);
-        board2 = new Board("000001", "My Board", null);
+        board2 = new Board("000001", "My Board", null, null);
         board2.addList(cardList);
     }
 
