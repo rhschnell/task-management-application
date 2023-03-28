c41d7ff672348ac5de49c2528f84922ad114e608
Ruben Schnell <r.h.schnell@student.tudelft.nl>
2023-03-28 22:40:03 +0200

    Checkstyle

--- client/src/main/java/client/windows/tags/add/AddTagService.java
+++ client/src/main/java/client/windows/tags/add/AddTagService.java
@@ -1,7 +1,6 @@
 package client.windows.tags.add;
 
 import client.serverUtils.BoardUtils;
-import client.windows.workspace.boardSpace.WorkspaceCtrl;
 import com.google.inject.Inject;
 import commons.Board;
 
