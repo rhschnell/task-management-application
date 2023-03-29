b6b9cfcedefeaa95cf0eef4dac225d28c66b2bcf
Yurian Lagrand <Y.G.Lagrand@student.tudelft.nl>
2023-03-29 12:29:04 +0200

    added javadoc on new methods

--- client/src/main/java/client/windows/workspace/rename/RenameCtrl.java
+++ client/src/main/java/client/windows/workspace/rename/RenameCtrl.java
@@ -15,28 +15,50 @@ public class RenameCtrl {
     @FXML
     private TextField inputField;
 
+    /**
+     * Injectable constructor for the RenameCtrl
+     * After using this constructor setWorkspaceCtrl MUST be called.
+     * @param service Injected parameter of corresponding service
+     */
     @Inject
     public RenameCtrl(RenameService service) {
         this.service = service;
     }
 
+    /**
+     * This method is executed upon clicking the save button in the popup.
+     * It sets the title of the board to the current text of the
+     * inputField and then inserts this into the database through
+     * the service.
+     * Then it closes the window.
+     */
     public void save() {
         Board board = workspaceCtrl.getShownBoard();
         board.setTitle(inputField.getText());
-        System.out.println(inputField.getText());
         service.insertBoard(board);
         close();
     }
 
+    /**
+     * This method is executed upon clicking the cancel button in the popup.
+     * It clears the inputField and closes the window.
+     */
     public void cancel() {
         inputField.clear();
         close();
     }
 
+    /**
+     * This method is used to close the window.
+     */
     private void close() {
         ((Stage)inputField.getScene().getWindow()).close();
     }
 
+    /**
+     * Setter for workspaceCtrl, MUST be called after constructor.
+     * @param workspaceCtrl the instance to be injected
+     */
     public void setWorkspaceCtrl(WorkspaceCtrl workspaceCtrl) {
         this.workspaceCtrl = workspaceCtrl;
         this.inputField.setText(workspaceCtrl.getShownBoard().getTitle());
--- client/src/main/java/client/windows/workspace/rename/RenameService.java
+++ client/src/main/java/client/windows/workspace/rename/RenameService.java
@@ -7,11 +7,19 @@ import commons.Board;
 public class RenameService {
     private final BoardUtils server;
 
+    /**
+     * Injectable constructor for the RenameService
+     * @param server the utility to communicate with the server
+     */
     @Inject
     public RenameService(BoardUtils server) {
         this.server = server;
     }
 
+    /**
+     * Insert given board into the database.
+     * @param board Board to be inserted
+     */
     public void insertBoard(Board board) {
         server.insertBoard(board);
     }
