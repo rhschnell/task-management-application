d66e5eb05a0f6ee20e42ed081054528df52d1c05
Yurian Lagrand <Y.G.Lagrand@student.tudelft.nl>
2023-03-30 18:33:15 +0200

    fix

--- client/src/main/java/client/windows/login/admin/AdminLoginService.java
+++ client/src/main/java/client/windows/login/admin/AdminLoginService.java
@@ -2,20 +2,24 @@ package client.windows.login.admin;
 
 import client.serverUtils.AdminUtils;
 import client.serverUtils.ServerUtils;
+import client.utils.HelperMethods;
 import com.google.inject.Inject;
 
 public class AdminLoginService {
     private final ServerUtils server;
     private final AdminUtils adminUtils;
+    private final HelperMethods hml;
 
     /**
      * Constructor for AdminLoginService
      * @param server a server util
+     * @param hml
      */
     @Inject
-    public AdminLoginService(ServerUtils server) {
+    public AdminLoginService(ServerUtils server, HelperMethods hml) {
         this.server = server;
         this.adminUtils = new AdminUtils(server);
+        this.hml = hml;
     }
 
     /**
@@ -24,6 +28,7 @@ public class AdminLoginService {
      */
     public boolean serverPing(String serverAddress) {
         server.setServer(serverAddress);
+        hml.setServerIP(serverAddress);
         return server.pingServer();
     }
 
