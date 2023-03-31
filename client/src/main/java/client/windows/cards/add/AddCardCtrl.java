21e10451d6147c83b7874e4471a25e1548fbe15b
ttanasescu <t.i.tanasescu-1@student.tudelft.nl>
2023-03-31 20:28:54 +0200

    Wroked on AddCard

--- client/src/main/java/client/windows/cards/add/AddCardCtrl.java
+++ client/src/main/java/client/windows/cards/add/AddCardCtrl.java
@@ -18,6 +18,8 @@ package client.windows.cards.add;
 import client.MyFXML;
 import client.modules.MainModules;
 import client.utils.HelperMethods;
+import client.windows.subtasks.SubtaskCellCtrl;
+import client.windows.subtasks.SubtaskContainer;
 import client.windows.tags.view.CustomTagCellCtrl;
 import client.windows.tags.view.TagListCtrl;
 import com.google.inject.Inject;
@@ -31,6 +33,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.util.ArrayList;
@@ -38,7 +42,7 @@ import java.util.List;
 
 import static com.google.inject.Guice.createInjector;
 
-public class AddCardCtrl {
+public class AddCardCtrl extends SubtaskContainer {
 
     private final AddCardService service;
 
@@ -151,6 +155,47 @@ public class AddCardCtrl {
         Scene scene = new Scene(root);
         String title = "Add tag";
         hm.popUp(scene, title);
+    }
+
+    /**
+     * Adds a new subtask to a card
+     */
+    public void addTask() {
+        if (taskList == null) {
+            taskList = new ArrayList<>();
+        }
+        if (!(addSubtaskTitle.getText() != null && !addSubtaskTitle.getText().isEmpty())) {
+            return; //TODO: notify user in some way that you cannot add empty tasks
+        }
+        Task newTask = new Task();
+        newTask.setCompleted(false);
+        newTask.setTitle(addSubtaskTitle.getText());
+        taskList.add(newTask);
+        addSubtaskTitle.clear();
+        displayTasks();
+    }
+
+    public void handleKeyPressed(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            addTask();
+        }
+    }
+
+    public void displayTasks() {
+        subtasks.getChildren().clear();
+        for (Task task : taskList) {
+
+            var loader = new MyFXML(createInjector()).load(SubtaskCellCtrl.class,
+                    "client", "windows", "cards", "subtasks", "SubtaskCell.fxml");
+            loader.getKey().updateItem(task);
+            loader.getKey().setSubtaskContainer(this);
+            subtasks.getChildren().add(loader.getValue());
+        }
+    }
+    @Override
+    public void deleteSubtask(Task task) {
+        taskList.remove(task);
+        displayTasks();
     }
 
 }
