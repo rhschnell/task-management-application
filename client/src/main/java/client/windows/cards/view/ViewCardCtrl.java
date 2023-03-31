9caa1b69fcde36fe5b0ac59088b17bbd8aec8a93
ttanasescu <t.i.tanasescu-1@student.tudelft.nl>
2023-03-31 20:45:20 +0200

    Worked on ViewCard

--- client/src/main/java/client/windows/cards/view/ViewCardCtrl.java
+++ client/src/main/java/client/windows/cards/view/ViewCardCtrl.java
@@ -20,9 +20,11 @@ import client.MyFXML;
 import client.MainCtrl;
 import client.utils.HelperMethods;
 import client.windows.cards.edit.EditCardCtrl;
+import client.windows.subtasks.SubtaskCellCtrl;
 import client.windows.tags.view.CustomTagCellCtrl;
 import com.google.inject.Inject;
 import commons.Card;
+import commons.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -56,11 +58,13 @@ public class ViewCardCtrl {
     @FXML
     private VBox appliedTagsVbox;
 
+    @FXML
+    private VBox taskBox;
+
     private ViewCardService service;
 
     /**
      * Constructor for ViewCardCtrl
-     * @param server a server util
      */
     @Inject
     public ViewCardCtrl(ViewCardService service, MainCtrl mainCtrl, MyFXML myFXML) {
@@ -144,5 +148,19 @@ public class ViewCardCtrl {
     public void setBoardKey(String boardKey)
     {
         service.setBoardKey(boardKey);
+    }
+
+    /**
+     * Allows us to add subtasks to a list
+     */
+    public void displayTasks() {
+        taskBox.getChildren().clear();
+        for (Task task : card.getSubTasks()) {
+            var loader = new MyFXML(createInjector()).load(SubtaskCellCtrl.class,
+                    "client", "windows", "cards", "subtasks", "SubtaskCell.fxml");
+            loader.getKey().updateItem(task);
+            loader.getKey().disableEdit();
+            taskBox.getChildren().add(loader.getValue());
+        }
     }
 }
