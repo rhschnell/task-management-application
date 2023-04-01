dc55aadf1e591d4bd878f0a346d03c044a4c10b7
Sander Bakker <s.a.bakker-4@student.tudelft.nl>
2023-04-01 21:52:23 +0200

    Created test for TagUtils and created reusable mocking component for testing HTTP request

--- client/src/main/java/client/serverUtils/TagUtils.java
+++ client/src/main/java/client/serverUtils/TagUtils.java
@@ -3,6 +3,7 @@ package client.serverUtils;
 import com.google.inject.Inject;
 import commons.Route;
 import commons.Tag;
+import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
@@ -14,34 +15,41 @@ import java.util.List;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class TagUtils {
-    private ServerUtils serverUtils;
+    private final ServerUtils serverUtils;
+    private Client client;
 
 
     /**
      * Creates a new TagUtils object
+     *
      * @param serverUtils The ServerUtils object (injected) to use in requests.
      */
     @Inject
     public TagUtils(ServerUtils serverUtils) {
+
         this.serverUtils = serverUtils;
+        this.client = ClientBuilder.newClient(new ClientConfig());
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
     }
 
     public void addTagToCard(Tag tag, long cardId) {
-        ClientBuilder.newClient(new ClientConfig())
-                .target(serverUtils.getServer()).path("api/tagToCard")
+        client.target(serverUtils.getServer()).path("api/tagToCard")
                 .queryParam("cardId", cardId)
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
                 .post(Entity.entity(tag, APPLICATION_JSON), Tag.class);
     }
 
-
     /**
      * Sends a post request to the server to add a tag to the database
+     *
      * @param tag The tag list to add
      */
     public void insertTag(Tag tag) {
-        ClientBuilder.newClient(new ClientConfig())
+        client
                 .target(serverUtils.getServer()).path(Route.TAG)
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
@@ -50,10 +58,11 @@ public class TagUtils {
 
     /**
      * Sends a request to the server to delete a certain tag from the database
+     *
      * @param id of the tag to delete
      */
     public void deleteTag(long id) {
-        ClientBuilder.newClient(new ClientConfig())
+        client
                 .target(serverUtils.getServer()).path(Route.TAG + "/" + id)
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
@@ -61,10 +70,11 @@ public class TagUtils {
     }
 
     public List<Tag> getBoardTags(String key) {
-        return ClientBuilder.newClient(new ClientConfig())
+        return client
                 .target(serverUtils.getServer()).path(Route.BOARD + "/getBoardTags/" + key)
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
-                .get(new GenericType<List<Tag>>() {});
+                .get(new GenericType<List<Tag>>() {
+                });
     }
 }
