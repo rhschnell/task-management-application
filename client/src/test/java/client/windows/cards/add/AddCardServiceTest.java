04d1b3606cf6cce171d49782fbc36a5e5767019c
Sander Bakker <s.a.bakker-4@student.tudelft.nl>
2023-04-02 00:59:25 +0200

    Created test for 100% coverage of AddCardService class

---
+++ client/src/test/java/client/windows/cards/add/AddCardServiceTest.java
@@ -0,0 +1,135 @@
+package client.windows.cards.add;
+
+import client.serverUtils.CardListUtils;
+import client.serverUtils.TagUtils;
+import commons.Card;
+import commons.CardList;
+import commons.Tag;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class AddCardServiceTest {
+
+    private AddCardService addCardService;
+    private CardListUtils server;
+    private TagUtils tagUtils;
+
+    @BeforeEach
+    void setUp() {
+        server = Mockito.mock(CardListUtils.class);
+        tagUtils = Mockito.mock(TagUtils.class);
+
+        when(tagUtils.getBoardTags(anyString())).thenReturn(List.of(
+                new Tag("Testing", "FFFFFF"),
+                new Tag("BugFixing", "000000"),
+                new Tag("Refactoring", "00FF00")
+        ));
+
+        addCardService = new AddCardService(server, tagUtils);
+    }
+
+    @Test
+    void getBoardKey() {
+        assertNull(addCardService.getBoardKey());
+    }
+
+    @Test
+    void setBoardKey() {
+        addCardService.setBoardKey("New key");
+        assertEquals("New key", addCardService.getBoardKey());
+    }
+
+    @Test
+    void getCardList() {
+        assertNull(addCardService.getCardList());
+    }
+
+    @Test
+    void setCardList() {
+        Card card1 = new Card();
+        Card card2 = new Card();
+        CardList cardList = new CardList();
+        cardList.addCard(card1);
+        cardList.addCard(card2);
+        addCardService.setCardList(cardList);
+        assertEquals(cardList, addCardService.getCardList());
+    }
+
+    @Test
+    void insertCardList() {
+        CardList toInsert = new CardList();
+        addCardService.setCardList(toInsert);
+        addCardService.insertCardList();
+        verify(server).insertCardList(toInsert);
+    }
+
+    @Test
+    void getTags() {
+        assertEquals(new ArrayList<Tag>(), addCardService.getTags());
+    }
+
+    @Test
+    void getAppliedTags() {
+        assertEquals(new ArrayList<Tag>(), addCardService.getAppliedTags());
+    }
+
+    @Test
+    void setAppliedTags() {
+        List<Tag> applied = List.of(
+                new Tag("Tag 1", "FFFFFF"), new Tag("Tag 2", "000000"));
+        addCardService.setAppliedTags(applied);
+        assertEquals(applied, addCardService.getAppliedTags());
+    }
+
+    @Test
+    void applyTag() {
+        Tag toApply = new Tag("Some title", "20CE00");
+        addCardService.applyTag(toApply);
+        assertEquals(List.of(toApply), addCardService.getAppliedTags());
+    }
+
+    @Test
+    void applyTagDuplicate() {
+        Tag toApply = new Tag("Some title", "20CE00");
+        addCardService.applyTag(toApply);
+        assertEquals(List.of(toApply), addCardService.getAppliedTags());
+
+        addCardService.applyTag(toApply);
+        assertEquals(List.of(toApply), addCardService.getAppliedTags());
+    }
+
+    @Test
+    void getAvailableTagsEmpty() {
+        List<Tag> available = addCardService.getAvailableTags();
+        verify(tagUtils).getBoardTags(addCardService.getBoardKey());
+        assertEquals(new ArrayList<Tag>(), available);
+    }
+
+    @Test
+    void getAvailableTags() {
+        List<Tag> boardTags = tagUtils.getBoardTags("some key");
+        addCardService.setBoardKey("some key");
+        addCardService.applyTag(boardTags.get(0));
+        assertEquals(boardTags.subList(1, boardTags.size()), addCardService.getAvailableTags());
+    }
+
+
+    @Test
+    void addCard() {
+        Card card = new Card();
+        CardList cardListMock = Mockito.mock(CardList.class);
+        addCardService.setCardList(cardListMock);
+        addCardService.addCard(card);
+        verify(cardListMock).addCard(card);
+    }
+}
