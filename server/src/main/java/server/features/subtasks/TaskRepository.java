868a5cb3d54798cdf5d7aa4072176bfea0ab914b
Yurian Lagrand <Y.G.Lagrand@student.tudelft.nl>
2023-04-09 11:23:53 +0200

    missing annotation added

--- server/src/main/java/server/features/subtasks/TaskRepository.java
+++ server/src/main/java/server/features/subtasks/TaskRepository.java
@@ -3,5 +3,6 @@ package server.features.subtasks;
 import commons.Task;
 import server.features.CustomRepository;
 
+@Repository
 public interface TaskRepository extends CustomRepository<Task, Long> {
 }
