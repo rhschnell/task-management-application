113f6f4d242ae9ee90b8ad2df767eb9a2063c4e5
Ruben Schnell <r.h.schnell@student.tudelft.nl>
2023-04-11 16:02:52 +0200

    Update DataFormatManagerTest.java

--- client/src/test/java/client/utils/DataFormatManagerTest.java
+++ client/src/test/java/client/utils/DataFormatManagerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DataFormatManagerTest {
     private DataFormatManager dataFormatManager;
